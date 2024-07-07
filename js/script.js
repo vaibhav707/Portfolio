e47f2b79bbeffba71a0265f5bd284255a0c2d3f4
Vaibhav <95583397+vaibhav707@users.noreply.github.com>
2024-07-07 21:40:27 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Active section manager
+let navLinks = document.querySelectorAll("header nav a");
+let sections = document.querySelectorAll("section");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+      document
+        .querySelector("header nav a[href*=" + id + "]")
+        .classList.add("active");
+    }
+  });
+
+  // toggle icon navbar
+  let menuIcon = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
+
+  menuIcon.onclick = function () {
+    menuIcon.classList.toggle("bx-x");
+    navbar.style.visibility = "visible";
+
+    window.onscroll = () => {
+      // remove navbar when a nav link is clicked
+      menuIcon.classList.remove("bx-x");
+      navbar.style.visibility = "hidden";
+    }
+  };
+};
+
+
+// scroll reveal 
+ScrollReveal({
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-img, .heading', { origin: 'top' });
+ScrollReveal().reveal('.portfolio-box, .services-container', { origin: 'bottom' });
